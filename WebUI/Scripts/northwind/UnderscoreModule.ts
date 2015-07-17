9463d3eb6447769dc52aab43ee073d0d15a6c45c
aburok <gringo.kromka@gmail.com>
2015-07-17 20:19:42 +0200

    delete conpiled js

--- WebUI/Scripts/northwind/UnderscoreModule.ts
+++
@@ -1,9 +0,0 @@
-﻿module Store {
-    var underscore = angular.module('Common', [])
-        .factory('underscore', [
-            // The function (lambda) must return non null value
-
-            // Else it will throw : 'must return $get'
-        '$window', $window => $window._
-    ]);
-}
