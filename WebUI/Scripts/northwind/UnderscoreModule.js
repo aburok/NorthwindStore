a4697a5b653237f3e7ab2f74b3abe905beae6663
aburok <gringo.kromka@gmail.com>
2015-07-17 20:26:34 +0200

    delete

--- WebUI/Scripts/northwind/UnderscoreModule.js
+++
@@ -1,8 +0,0 @@
-var Store;
-(function (Store) {
-    var underscore = angular.module('Common', []).factory('underscore', [
-        '$window',
-        function ($window) { return $window._; }
-    ]);
-})(Store || (Store = {}));
-//# sourceMappingURL=UnderscoreModule.js.map
